11db567e2f72e20ba5873a1b10149d6c6676b27a
Krystian <titotecatito@proton.me>
2024-07-08 12:00:09 +0200

    editing & styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,15 @@ import '../scss/_reset.scss';
 import '../scss/_fonts.scss';
 import '../scss/_globals.scss';
 import '../scss/_mixins.scss';
+import '../scss/_nav.scss';
+import '../scss/_header.scss';
+import '../scss/_about.scss';
+
+const burgerBtn = document.getElementById('burgerBtn');
+const navMobile = document.getElementById('navMobile');
+const navDesktop = document.getElementById('navDesktop');
+
+burgerBtn.addEventListener('click', () => {
+  navMobile.classList.toggle('active');
+  //   console.log('essa');
+});
