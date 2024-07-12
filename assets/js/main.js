5c52f8b5b8e4b86ac2a00773b7dc307a6adbb2c5
Krystian <titotecatito@proton.me>
2024-07-12 12:01:40 +0200

    adding new sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,21 @@ import '../scss/_about.scss';
 import '../scss/_offersSection.scss';
 import '../scss/_parallax.scss';
 import '../scss/_animations.scss';
+import '../scss/_footer.scss';
 
 const burgerBtn = document.getElementById('burgerBtn');
 const burgerBtnMid = document.getElementById('burgerBtnMid');
 const burgerBtnBot = document.getElementById('burgerBtnBot');
 const navMobile = document.getElementById('navMobile');
 const navDesktop = document.getElementById('navDesktop');
+const footerInfo = document.getElementById('footerInfo');
 
 burgerBtn.addEventListener('click', () => {
   navMobile.classList.toggle('active');
   burgerBtnMid.classList.toggle('burgerActive');
   burgerBtnBot.classList.toggle('burgerActive');
 });
+
+const date = new Date();
+console.log(date);
+footerInfo.innerHTML = `&copy ${date.getFullYear()} Forest Group Company Ltd`;
